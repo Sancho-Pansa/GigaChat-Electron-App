7ecb6b42620aa8ebde3dc36a6ede33d46c6fa854
SanchoPansa <zelgadis4@gmail.com>
2025-04-29 17:59:44 +0300

    Configs

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,10 @@ import globals from "globals";
 import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginVue.configs["flat/essential"]
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  pluginVue.configs["flat/essential"],
 ]);
