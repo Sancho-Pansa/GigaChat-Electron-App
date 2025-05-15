9af900cbd6a5a7ddc282441121834aa7bfc84885
SanchoPansa <zelgadis4@gmail.com>
2025-05-15 17:26:09 +0300

    Distinct header block

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import ChatWindow from "./frontend/ChatWindow.vue";
 import FooterInfo from "./frontend/FooterInfo.vue";
+import HeaderInfo from "./frontend/HeaderInfo.vue";
 </script>
 
 <template>
   <div class="min-h-screen max-h-screen flex flex-col m-0 p-0">
-    <h1 class="font-bold text-center text-2xl">Welcome to Electron GigaChat 🤖</h1>
+    <HeaderInfo></HeaderInfo>
     <ChatWindow class="grow"></ChatWindow>
     <FooterInfo></FooterInfo>
   </div>
---
+++ src/frontend/HeaderInfo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="grow-0 shrink-0 font-bold text-center text-2xl">Electron-Vue GigaChat 👤</h1>
+  </div>
+</template>
